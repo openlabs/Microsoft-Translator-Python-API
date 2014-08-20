c33a2c238653724b2b235f5c002f34ca40822717
Sharoon Thomas <sharoon.thomas@openlabs.co.in>
2014-08-20 19:17:00 +0530

    Use codecs to open README in setup.py
    Fixes #13

--- setup.py
+++ setup.py
@@ -27,11 +27,14 @@
     :license: BSD, see LICENSE for more details.
 """
 import os
+import codecs
 from setuptools import setup
 
 
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    return codecs.open(
+        os.path.join(os.path.dirname(__file__), fname)
+    ).read()
 
 setup(
     name="microsofttranslator",
